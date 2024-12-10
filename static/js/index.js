5d9cbef06f3ca59f8d2095aab1a9b629207d2353
Ruicheng Wang <wangrc2018cs@mail.ustc.edu.cn>
2024-12-10 17:07:53 +0800

    new homepage

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const starCountElements = document.querySelectorAll('span.star-count');
 
-    const repoUrl = "https://api.github.com/repos/Microsoft/TRELLIS";
-    const starCountElement = document.getElementById('star-count');
+    starCountElements.forEach(element => {
+        const repo = element.getAttribute('repo');
+        const repoUrl = `https://api.github.com/repos/${repo}`;
 
-    fetch(repoUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const starCount = data.stargazers_count;
-            starCountElement.textContent = `| ${starCount} Stars`;
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-            starCountElement.textContent = '| Stars not available';
-        });
+        fetch(repoUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const starCount = data.stargazers_count;
+                if (starCount > 1000)
+                    element.textContent = `${(starCount / 1000).toFixed(1)}k`;
+                else
+                    element.textContent = starCount;
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+                element.textContent = '-';
+            });
+    });
 });
